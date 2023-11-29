547777bf0e3f84e5bc2bf5451a60a1265b21cf5b
Xiahui Liu <coffeeliuxiahui@gmail.com>
2023-11-29 11:37:17 +0100

    Add game mapping

--- Tournament.Core/Dto/GameDto.cs
+++ Tournament.Core/Dto/GameDto.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Tournament.Core.Dto
 {
     public class GameDto
     {
-        public string? Title { get; set; }
-        public DateTime StartDate { get; set; }
+        public Guid Id { get; init; }
+        public string? Title { get; init; }
+        public DateTime Time { get; init; }
     }
 }
---
+++ Tournament.Data/GameMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Tournament.Core.Dto;
+using Tournament.Core.Entities;
+
+namespace Tournament.Data
+{
+    public class GameMapping : Profile
+    {
+        public GameMapping()
+        {
+            CreateMap<Game, GameDto>()
+           .ForMember(
+           dest => dest.Title,
+           from => from.MapFrom(g => g.Title));
+        }
+       
+        
+    }
+}
